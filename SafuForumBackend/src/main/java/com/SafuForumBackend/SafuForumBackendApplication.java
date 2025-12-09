adb8893639e0ef2cc42eca3ee559e13a5a866dc5
cleon <cleontann@gmail.com>
2025-12-09 16:48:11 +0800

    add spring boot starter files

---
+++ SafuForumBackend/src/main/java/com/SafuForumBackend/SafuForumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.SafuForumBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafuForumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafuForumBackendApplication.class, args);
+	}
+
+}
