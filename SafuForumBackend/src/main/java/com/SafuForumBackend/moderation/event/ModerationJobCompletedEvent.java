a0004c36c03d01e72174e2079eedfff40586d00f
Ryann Goh <ryanngohenrui@gmail.com>
2025-12-20 16:48:50 +0800

    Update

--- SafuForumBackend/src/main/java/com/SafuForumBackend/moderation/event/ModerationJobCompletedEvent.java
+++ SafuForumBackend/src/main/java/com/SafuForumBackend/moderation/event/ModerationJobCompletedEvent.java
@@ -2,7 +2,8 @@ package com.SafuForumBackend.moderation.event;
 
 import com.SafuForumBackend.moderation.enums.ModerationStatus;
 
-public record ModerationPostCompletedEvent(
+public record ModerationJobCompletedEvent(
+        Long moderationJobId,
         Long postId,
         Integer postVersion,
         ModerationStatus status,
